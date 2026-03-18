033d7f2fe38d551b5d51d10d0bab470ecae3e88d
zxcasdqwert2007-ui <zxcasdqwert2007@gmail.com>
2026-03-18 14:18:54 +0300

    zaebalo

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("8677688958:AAGu54n6o4BwOt2UaUg0_wcNuTjhrQQo2rQ")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 # API ID и Hash для Telethon (если будем проверять онлайн через Telegram - это сложно, пока опустим)
 # TELEGRAM_API_ID = os.getenv("TELEGRAM_API_ID")
 # TELEGRAM_API_HASH = os.getenv("TELEGRAM_API_HASH")
